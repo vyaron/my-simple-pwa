6b95cceabf06dc894b188c200a1302b60fd314da
vyaron <vyaron@gmail.com>
2019-03-19 12:38:12 +0200

    deploy

---
+++ precache-manifest.3f09ed6bf9643b4751e3611648673a15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "26fee0059ec098d5f609",
+    "url": "/my-simple-pwa/js/about.d288b4f1.js"
+  },
+  {
+    "revision": "51d96b2af1b201fa66a8",
+    "url": "/my-simple-pwa/css/app.ac906db3.css"
+  },
+  {
+    "revision": "51d96b2af1b201fa66a8",
+    "url": "/my-simple-pwa/js/app.83a99363.js"
+  },
+  {
+    "revision": "b481acc781da09280082",
+    "url": "/my-simple-pwa/js/chunk-vendors.2e4ab3f2.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/my-simple-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "984aad927a9baceb7bf0e2deb360a31c",
+    "url": "/my-simple-pwa/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-simple-pwa/robots.txt"
+  }
+];
